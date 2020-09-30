abf00f4bba6731291a28f9643bc39436785b865b
Luc Wendel <45206673+LucWendel@users.noreply.github.com>
2020-09-30 10:29:35 +0200

    taak 1

--- 1-Arrays/taak01/index.php
+++ 1-Arrays/taak01/index.php
@@ -1,1 +1,24 @@
+<?php
 
+$medewerkers = [
+    [
+      'voornaam' => 'Willem',
+      'achternaam' => 'van Oranje',
+      'functie'  => 'koning'
+    ],
+    [
+      'voornaam' => 'Donald',
+      'achternaam' => 'Trump',
+      'functie'  => 'president'
+    ]
+    ]
+    ;
+
+?>
+    <?php  foreach($medewerkers as $row): ?>
+      <tr>
+          <td><?=$row['voornaam'];?></td>
+          <td><?=$row['achternaam'];?></td>
+          <td><?=$row['functie'];?></td>
+      </tr>
+<?php endforeach;?>
